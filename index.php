7c0fe3eeb7e34a096fcfda7252a3fe493fb3a8c5
Hoss MOHAMMADI <1386.hossein.mohammadi@gmail.com>
2024-05-30 09:52:34 -0700

    Router-Finished

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 # front conroller
 
-use App\Utilities\Lang;
-
+use App\Core\StupidRouter;
 include "bootsrap/init.php";
 
-$text = "در سال 1400 سون لرن بیش از 100 هزار کاربر ثبت نامی دارد";
-echo Lang::persian_numbers($text);
+
+$router = new StupidRouter();
+$router->run();
+
+
